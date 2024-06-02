0b1f8d92f4883331a34d85740d180fd408b20ef6
AnthonyNunez500 <u202119276@upc.edu.pe>
2024-06-01 23:30:38 -0500

    feat reminder

--- lib/reminders/presentation/medicine/widgets/add_new_reminder_screen.dart
+++ lib/reminders/presentation/medicine/widgets/add_new_reminder_screen.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_application/common/styles/styles.dart';
-
 class AddNewReminderScreen extends StatelessWidget {
   const AddNewReminderScreen({Key? key}) : super(key: key);
 
@@ -9,6 +6,190 @@ class AddNewReminderScreen extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    return Text('Add New Reminder Screen');
+    return AlertDialog(
+      backgroundColor: Colors.white,
+      title: Center(
+        child: Text('Add New Reminder',
+            style: TextStyle(
+              fontFamily: Styles.headingFont,
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+              color: Styles.primaryColor,
+            )),
+      ),
+      content: Flexible(
+        child: SingleChildScrollView(
+            child: Column(children: [
+          Padding(
+            padding: EdgeInsets.only(right: screenWidth * 0.35),
+            child: Text(
+              'Medicine Name',
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: Styles.headingFont,
+                fontWeight: FontWeight.bold,
+                color: Styles.primaryColor,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            child: TextFormField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Enter Medicine Name',
+                prefixIcon: Icon(
+                  Icons.medical_information,
+                  color: Styles.primaryColor,
+                ),
+                contentPadding: const EdgeInsets.all(20),
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Styles.primaryColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.only(right: screenWidth * 0.40),
+            child: Text(
+              'Dosage(mg)',
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: Styles.headingFont,
+                fontWeight: FontWeight.bold,
+                color: Styles.primaryColor,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            child: TextFormField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Enter Dosage',
+                prefixIcon: Icon(
+                  Icons.mode_rounded,
+                  color: Styles.primaryColor,
+                ),
+                contentPadding: const EdgeInsets.all(20),
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Styles.primaryColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.only(right: screenWidth * 0.40),
+            child: Text(
+              'Frequency',
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: Styles.headingFont,
+                fontWeight: FontWeight.bold,
+                color: Styles.primaryColor,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            child: TextFormField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                hintText: 'Enter Frequency',
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Styles.primaryColor,
+                ),
+                contentPadding: const EdgeInsets.all(20),
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Styles.primaryColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.only(right: screenWidth * 0.50),
+            child: Text(
+              'Time',
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: Styles.headingFont,
+                fontWeight: FontWeight.bold,
+                color: Styles.primaryColor,
+              ),
+            ),
+          ),
+        ])),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 150,
+              height: 45,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.black26,
+                ),
+                child: const Text('Cancel'),
+              ),
+            ),
+            const SizedBox(width: 5),
+            SizedBox(
+              width: 150,
+              height: 45,
+              child: TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Styles.primaryColor,
+                ),
+                child: const Text('Confirm'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
