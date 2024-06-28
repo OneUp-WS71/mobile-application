7abd3a40945b326fca47b7546886dd07b529320c
Cristhian Orosco <u201921031@upc.edu.pe>
2024-06-27 19:30:30 -0500

    feat: update location screen

--- lib/dataManagment/presentation/location/location_screen.dart
+++ lib/dataManagment/presentation/location/location_screen.dart
@@ -5,15 +5,12 @@ import 'package:mobile_application/common/styles/styles.dart';
 import 'package:mobile_application/common/widgets/custom_app_bar.dart';
 import 'package:mobile_application/common/widgets/navigation_bar.dart';
 
-import '../../../injections.dart';
-import '../../application/use_cases/get_report_by_id.dart';
 
 const MapboxAccessToken = 'pk.eyJ1IjoiY3Jpc29yczA5IiwiYSI6ImNseHMzcGIzMDE2dXQyd3BrbzRxZDJpemkifQ.DflG0_KHMBjn7c26appWIg';
 const myPosition = LatLng(-12.076832832450908, -77.09348179398481); // Coordenadas de Lima, Perú
 
 class LocationScreen extends StatefulWidget {
-  final int? userId; // parámetro opcional
-  const LocationScreen({Key? key,  this.userId}) : super(key: key);
+  const LocationScreen({Key? key}) : super(key: key);
 
   @override
   _LocationScreenState createState() => _LocationScreenState();
@@ -21,35 +18,13 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   late MapController _mapController;
-   double? latitude;
-   double? longitude;
-
   @override
   void initState() {
     super.initState();
     _mapController = MapController();
-    latitude = myPosition.latitude;
-    longitude = myPosition.longitude;
-    _fetchReportData();
   }
-
-  Future<void> _fetchReportData() async {
-    try {
-      //final report = Provider.of<UserModel>(context).report;
-      final report = await serviceLocator<GetReportById>()(widget.userId!);
-      setState(() {
-        latitude = double.parse(report.latitude!);
-        longitude = double.parse(report.longitude!);
-      });
-    } catch (e) {
-      print('Failed to fetch report: $e');
-    }
-  }
-
   void _gotoMyPosition() {
-    if (latitude != null && longitude != null) {
-      _mapController.move(LatLng(latitude!, longitude!), 15.0);
-    } // mover a myposition
+    _mapController.move(myPosition, 15.0); // mover a myposition
   }
 
   @override
@@ -59,13 +34,11 @@ class _LocationScreenState extends State<LocationScreen> {
         title: 'Location',
       ),
       bottomNavigationBar: const NavigationMenu(currentPageIndex: 2,),
-      body:
-      latitude != null && longitude != null ?
-      FlutterMap(
+      body: FlutterMap(
         mapController: _mapController,
         options: MapOptions(
-           initialCenter: LatLng(latitude!, longitude!),
-            initialZoom: 15.0,
+          initialCenter: myPosition,
+          initialZoom: 15.0,
           interactionOptions: InteractionOptions(
             flags: InteractiveFlag.all, // Habilita todas las interacciones por defecto
             rotationThreshold: 20.0,
@@ -87,7 +60,7 @@ class _LocationScreenState extends State<LocationScreen> {
           MarkerLayer(
             markers:[
               Marker(
-                point: LatLng(latitude!, longitude!),
+                point: myPosition,
                 width: 80.0,
                 height: 80.0,
                 child: Icon(
@@ -95,25 +68,21 @@ class _LocationScreenState extends State<LocationScreen> {
                   size: 60,
                   color: Styles.primaryColor,
                 ),
-                )
+              )
             ],
           ),
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: _gotoMyPosition,
-              child: Icon(
-                  Icons.my_location_rounded,
-                  color: Styles.primaryColor,
-                size: 30,
-            ),
-          )
-          )
         ],
-      )
-      : const Center(child: CircularProgressIndicator()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _gotoMyPosition,
+        backgroundColor: Colors.white,
+        child: Icon(
+          Icons.my_location_rounded,
+          color: Styles.primaryColor,
+          size: 30,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
